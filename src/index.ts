0145cf1ea300a806df1ef581be97c7af3733e26a
Rikky <son.levuthai@gmail.com>
2020-10-22 15:00:17 +0700

    re-export nestjs modules into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Subscriber, MessageShape, Transport } from './BroadcastContracts'
 import Broadcast from './Broadcast'
+import * as nestjs from './nestjs'
 
-export { Subscriber, MessageShape, Transport, Broadcast }
+export { Subscriber, MessageShape, Transport, Broadcast, nestjs }
